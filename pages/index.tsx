fe9b4cbc57b50e8ccf998b8501d81181865c195a
tiffany <ting11222001@gmail.com>
2023-01-18 22:00:52 +1030

    feat: 🎸 set up draft structure of the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = () => {
   return (
     <>
-      HeroBanner
+      <HeroBanner></HeroBanner>
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
-        <p>Plants of many variations</p>
+        <p>Plants of many kinds</p>
       </div>
 
       <div className="products-container">
         {['Product1', 'Product2'].map((product) => product)}
       </div>
 
-      Footer
+      <FooterBanner></FooterBanner>
     </>
   )
 }
