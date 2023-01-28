af479681dfa5585c6d5851a9fe788757cd78e985
tiffany <litingliaotiff@gmail.com>
2023-01-28 13:50:19 +1030

    feat: 🎸 create add to cart logic

--- context/StateContext.jsx
+++ context/StateContext.jsx
@@ -1,11 +1,13 @@
+import { privateDecrypt } from 'crypto';
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { toast } from 'react-hot-toast';
+import { isTemplateExpression } from 'typescript';
 
 const Context = createContext();
 
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
-  const [cartItems, setCartItems] = useState();
+  const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState();
   const [totalQuantities, setTotalQuantities] = useState();
   const [qty, setQty] = useState(1);
@@ -21,10 +23,33 @@ export const StateContext = ({ children }) => {
     });
   }
 
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find((item) => item._id === product._id);
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id) return {
+          ...cartProduct,
+          quantity: cartProduct.quantity + quantity
+        }
+      })
+
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product }]);
+    }
+
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  }
+
   return (
     <Context.Provider
       value={{
-        showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty
+        showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty, onAdd
       }}
     >
       {children}
