68ceb05c9cc7e124b232f1be6ffcf77a30ef5bb6
tiffany <litingliaotiff@gmail.com>
2023-01-28 22:05:18 +1030

    feat: 🎸 add shipping rate

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -3,8 +3,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1MVCcLDpylY71kW8KII2rDZE' },
+          { shipping_rate: 'shr_1MVCebDpylY71kW8pqGPa2uV' },
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -15,7 +22,11 @@ export default async function handler(req, res) {
         mode: 'payment',
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
-      });
+      }
+
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
       res.redirect(303, session.url);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
