18ac429782ea099c246c0f60f65dcdadf5c63ede
tiffany <litingliaotiff@gmail.com>
2023-01-28 17:40:54 +1030

    feat: 🎸 add the close functionality on the cart heading

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart, showCart } = useStateContext();
+
   return (
-    <div>Cart</div>
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+
+      </div>
+    </div >
   )
 }
 
