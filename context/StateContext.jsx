f0f2e4b90efaa62e83a517369ee43ba42e898d53
tiffany <litingliaotiff@gmail.com>
2023-01-28 17:35:44 +1030

    fix: 🐛 update the export value from context provider

--- context/StateContext.jsx
+++ context/StateContext.jsx
@@ -6,8 +6,8 @@ const Context = createContext();
 export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false);
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
-  const [totalQuantities, setTotalQuantities] = useState();
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalQuantities, setTotalQuantities] = useState(0);
   const [qty, setQty] = useState(1);
 
   const incQty = () => {
@@ -47,7 +47,19 @@ export const StateContext = ({ children }) => {
   return (
     <Context.Provider
       value={{
-        showCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty, onAdd
+        showCart,
+        cartItems,
+        totalPrice,
+        totalQuantities,
+        qty,
+        incQty,
+        decQty,
+        onAdd,
+        setShowCart,
+        setCartItems,
+        setTotalPrice,
+        setTotalQuantities,
+        setQty
       }}
     >
       {children}
