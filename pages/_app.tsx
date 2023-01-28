ba9b815a73f221d24d4df239a598837044aec37d
tiffany <litingliaotiff@gmail.com>
2023-01-28 12:59:02 +1030

    feat: 🎸 add toaster to the root app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Layout } from '../components';
 import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <StateContext>
       <Layout>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
