7b54704338051bac8fc9a5535597675675f29cf2
tiffany <litingliaotiff@gmail.com>
2023-01-29 18:31:31 +1030

    feat: 🎸 add cancel route and page

---
+++ pages/cancel.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+
+import { useStateContext } from '../context/StateContext';
+
+const Cancel = () => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+  }, []);
+
+  return (
+    <div className="cancel-wrapper">
+      <div className="cancel">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Purchase cancelled!</h2>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Cancel
